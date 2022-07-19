324cbd55493242ed10ac19e96b13cc0b90662a8b
toufik2015 <tnefouci@gmail.com>
2022-07-19 17:13:23 +0100

    implementing Bootstrap Modal

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { Provider } from "react-redux";
 import store from "./store/index";
 import "./index.css";
 import App from "./App";
+import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
